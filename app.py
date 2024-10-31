614919b68dcaf3150db759977326b9eb9c390faa
dastarruer <ayushpramanik399@gmail.com>
2024-10-31 16:12:01 -0400

    Added database

--- app.py
+++ app.py
@@ -7,8 +7,10 @@ TASKS = []
 @app.route('/', methods=['GET','POST'])
 def index():
     if request.method == 'POST':
+        # Get the title and description of the task the user wants to add
         title = request.form['title']
         description = request.form['description']
-        TASKS.append({"title" : title, "description" : description})    
-    return render_template("index.html", tasks=TASKS)
 
+        # Append the task to TASKS
+        TASKS.append({"title" : title, "description" : description})
+    return render_template("index.html", tasks=TASKS)
