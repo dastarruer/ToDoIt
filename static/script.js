1eb23addf05cfab15745dfe342d2ecc8b128e932
dastarruer <ayushpramanik399@gmail.com>
2024-10-31 12:39:18 -0400

    Added a button to add tasks, however it is not functional right now

--- static/script.js
+++ static/script.js
@@ -3,7 +3,10 @@ for (let checkbox of checkboxes) {
     checkbox.addEventListener("click", (event) => {
         task = checkbox.parentNode;
 
-        // If the box has been checked, and the task is not finished, set it to a finished task. Otherwise, set it to an unfinished task
-        task.className =  task.className === "unfinished-task" ? "finished-task" : "unfinished-task";
+        // If the task is not finished, set it to a finished task. Otherwise, set it to an unfinished task
+        task.className =
+            task.className === "unfinished-task"
+                ? "finished-task"
+                : "unfinished-task";
     });
 }
