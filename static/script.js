5fe7660fd844cd8ac0745c89b0bbde7e7b407d81
dastarruer <ayushpramanik399@gmail.com>
2024-10-31 12:25:00 -0400

    Added script.js to change a task's class to finished or unfinished depending on whether the box has been checked

---
+++ static/script.js
@@ -0,0 +1,9 @@
+checkboxes = document.getElementsByClassName("checkbox");
+for (let checkbox of checkboxes) {
+    checkbox.addEventListener("click", (event) => {
+        task = checkbox.parentNode;
+
+        // If the box has been checked, and the task is not finished, set it to a finished task. Otherwise, set it to an unfinished task
+        task.className =  task.className === "unfinished-task" ? "finished-task" : "unfinished-task";
+    });
+}
