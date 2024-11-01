5b9db44bcdef9706cd1425af55db83f311e62c8e
dastarruer <ayushpramanik399@gmail.com>
2024-11-01 16:42:47 -0400

    The database now stores tasks, and display's the user's tasks dynamically

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+import sqlite3
 
 app = Flask(__name__)
 
-TASKS = []
+user_id = 1
+
+
+def get_db_connection():
+    """Return a connection to the database"""
+    conn = sqlite3.connect('tasks.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 
 @app.route('/', methods=['GET','POST'])
 def index():
+    conn = get_db_connection()
+    db = conn.cursor()
     if request.method == 'POST':
         # Get the title and description of the task the user wants to add
         title = request.form['title']
         description = request.form['description']
 
-        # Append the task to TASKS
-        TASKS.append({"title" : title, "description" : description})
-    return render_template("index.html", tasks=TASKS)
+        # Add the task to the database
+        db.execute("INSERT INTO tasks (user_id, title, description) VALUES(?, ?, ?)", (user_id, title, description))
+
+        conn.commit()
+
+    tasks = db.execute("SELECT title, description FROM tasks WHERE user_id = ?", [user_id]).fetchall()
+
+    conn.close()
+
+    return render_template("index.html", tasks=tasks)
--- tasks.sql
+++ tasks.sql
@@ -1,16 +1,16 @@
 -- This is just to write out sql commands before pasting it into the terminal
 
-CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL,
-    hash TEXT NOT NULL
-);
+-- CREATE TABLE users (
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     username TEXT NOT NULL,
+--     hash TEXT NOT NULL
+-- );
 
-CREATE TABLE tasks (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER NOT NULL,
-    title TEXT NOT NULL,
-    description TEXT,
-    completed INTEGER NOT NULL DEFAULT 0,
-    FOREIGN KEY (user_id) REFERENCES users(id)
-);
+-- CREATE TABLE tasks (
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     user_id INTEGER NOT NULL,
+--     title TEXT NOT NULL,
+--     description TEXT,
+--     completed INTEGER NOT NULL DEFAULT 0,
+--     FOREIGN KEY (user_id) REFERENCES users(id)
+-- );
