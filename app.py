87b577df6739fa4f931808ae4b6f258518b66e32
dastarruer <ayushpramanik399@gmail.com>
2024-11-02 22:33:11 -0400

    Login page is now functional, and allows the page to remember which user is logged in

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, redirect, request, session, url_for
 import sqlite3
 from flask_session import Session
-from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
@@ -9,13 +9,17 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-USER_ID = 1
-
 
 def isNull(parameter):
     if not parameter:
         return True
     return False
+
+
+def isUserValid(user: dict, password: str):
+    if len(user) < 1 or not check_password_hash(user["hash"], password):
+        return False
+    return True
 
 
 def get_db_connection():
@@ -40,19 +44,20 @@ def index():
             return redirect("/")
 
         # Add the task to the database
-        db.execute("INSERT INTO tasks (user_id, title, description) VALUES(?, ?, ?)", (USER_ID, title, description))
+        db.execute("INSERT INTO tasks (user_id, title, description) VALUES(?, ?, ?)", (session["user_id"], title, description))
 
         conn.commit()
 
         # This ensures that the browser does not ask for form resubmission after reloading the page
         return redirect(url_for("index"))
 
-    tasks = db.execute("SELECT title, description FROM tasks WHERE user_id = ?", [USER_ID]).fetchall()
+    tasks = db.execute("SELECT title, description FROM tasks WHERE user_id = ?", [session["user_id"]]).fetchall()
 
     conn.close()
 
     return render_template("index.html", tasks=tasks)
 
+# TODO: Return an error if there is a duplicate username
 @app.route("/register", methods=['GET','POST'])
 def register():
     if request.method == "POST":
@@ -78,9 +83,43 @@ def register():
         db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", (username, hash))
 
         # Finalize the transaction
-        conn.commit()        
+        conn.commit() 
+
+        conn.close()  
+
+        return render_template("login.html")     
     return render_template("register.html")
 
 @app.route("/login", methods=['GET','POST'])
 def login():
+    # Forget user id
+    session.clear()
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        
+        if isNull(username) or isNull(password):
+            error = "Username/Password cannot be empty"
+            return render_template("login.html", error=error)
+
+        conn = get_db_connection()
+        db = conn.cursor()
+
+        # Query database for username
+        user = db.execute("SELECT * FROM users WHERE username = ?", (username,)).fetchall()[0]
+
+        conn.close()
+
+        if not isUserValid(user, password):
+            error = "Invalid credentials, please try again"
+            return render_template("login.html", error=error)
+        
+        # Set the user id to the current user's id, so that the page remembers them
+        session["user_id"] = user["id"]
+
+        return redirect("/")
     return render_template("login.html")
+
+if __name__ == "__app.py__":
+    app.run(debug=True)
